591e83bcaa2fd32da1898469eacacdba1216542f
Lito <hipolito.arias.cervero@ing.com>
2018-06-14 15:35:25 +0200

    feature: add Row class

---
+++ Test WatchKit Extension/Row.swift
@@ -0,0 +1,13 @@
+//
+//  Row.swift
+//  ComunicationWatch WatchKit Extension
+//
+//  Created by Hipólito Arias on 14/06/2018.
+//  Copyright © 2018 MasterApps. All rights reserved.
+//
+
+import WatchKit
+
+class Row: NSObject {
+    @IBOutlet var lbl: WKInterfaceLabel!
+}
